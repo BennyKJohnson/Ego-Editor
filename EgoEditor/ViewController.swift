3ef5ac361c3ada0df71d71b52a6e1701829ef765
Benjamin Johnson <bkj986@uowmail.edu.au>
2015-10-01 17:35:42 +1000

    Initial Commit

---
+++ EgoEditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EgoEditor
+//
+//  Created by Benjamin Johnson on 1/10/2015.
+//  Copyright © 2015 Benjamin Johnson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
